6b08fae40bb4cd92afe6444bcce098e162d7b357
Jakub Jirutka <jakub@jirutka.cz>
2021-01-18 21:35:01 +0100

    Fix TypeScript type declarations to use accurate export style
    The current way is inaccurate because `anylogger.cjs.js`, the `main`
    entrypoint, uses CommonJS and does not export `default`. Thus it works
    only when the library consumer enables `esModuleInterop`. Most devs have
    it enabled, but this option is just a workaround for badly written
    type declarations.
    
    https://github.com/DefinitelyTyped/DefinitelyTyped#a-package-uses-export--but-i-prefer-to-use-default-imports-can-i-change-export--to-export-default:
    
    > For an npm package, export = is accurate if `node -p 'require("foo")'`
    > works to import a module, and export default is accurate if
    > `node -p 'require("foo").default'` works to import a module.

--- anylogger.d.ts
+++ anylogger.d.ts
@@ -1,88 +1,94 @@
-export type BaseLevels = {
-  error: 1,
-  warn: 2,
-  info: 3,
-  log: 4,
-  debug: 5,
-  trace: 6,
+declare namespace anylogger {
+  // NOTE: All types in this scope are implicitly exported.
+
+  type BaseLevels = {
+    error: 1,
+    warn: 2,
+    info: 3,
+    log: 4,
+    debug: 5,
+    trace: 6,
+  }
+
+  interface BaseLogger<L extends BaseLevels = BaseLevels> {
+    /**
+     * The name of this logger.
+     */
+    name: string
+
+    (level: keyof L, message?: any, ...args: any[]): void
+    (message?: any, ...args: any[]): void
+
+    error(message?: any, ...args: any[]): void
+    warn(message?: any, ...args: any[]): void
+    info(message?: any, ...args: any[]): void
+    log(message?: any, ...args: any[]): void
+    debug(message?: any, ...args: any[]): void
+    trace(message?: any, ...args: any[]): void
+    enabledFor(level: keyof L): boolean
+  }
+
+  type Logger<L extends BaseLevels = BaseLevels> = BaseLogger<L> & {
+    [P in keyof Omit<L, keyof BaseLevels>]: (message?: any, ...args: any[]) => void
+  }
+
+  interface AnyLogger<L extends BaseLevels, T extends BaseLogger<L> = Logger<L>> {
+    /**
+     * Returns an object containing all loggers created so far, keyed by name.
+     */
+    (): { [name: string]: T }
+
+    /**
+     * @param name The name of the new logger.
+     * @param config An optional config object.
+     * @returns A logger with the given `name` and `config`.
+     */
+    (name: string, config?: object | undefined): T
+
+    /**
+     * An object containing a mapping of level names to level values.
+     */
+    levels: L & { [name: string]: number }
+
+    /**
+     * Creates a new logger function that calls `anylogger.log` when invoked.
+     *
+     * @param name The name of the new logger.
+     * @param config An optional config object.
+     * @returns A new logger function with the given `name`.
+     */
+    new(name: string, config?: object | undefined): T
+
+    /**
+     * The log function used by `anylogger.new`.
+     *
+     * @param name The name of the logger to use
+     * @param level The log level.
+     * @param message The (formatted) message or any data to log.
+     * @param [params] Additional log (formatting) arguments.
+     */
+    log(name: string, level: keyof L, message?: any, ...args: any[]): void
+
+    /**
+     * The log function used by `anylogger.new`.
+     *
+     * @param name The name of the logger to use
+     * @param args The log arguments.
+     * @param message The (formatted) message or any data to log.
+     * @param [params] Additional log (formatting) arguments.
+     */
+    log(name: string, message?: any, ...args: any[]): void
+
+    /**
+     * @param logger The logger that should be (re-)extended.
+     * @return The logger that was given, extended.
+     */
+    ext(logger: T): T
+  }
 }
 
-export interface BaseLogger<L extends BaseLevels = BaseLevels> {
-  /**
-   * The name of this logger.
-   */
-  name: string
+declare const anylogger: anylogger.AnyLogger<anylogger.BaseLevels>
 
-  (level: keyof L, message?: any, ...args: any[]): void
-  (message?: any, ...args: any[]): void
-
-  error(message?: any, ...args: any[]): void
-  warn(message?: any, ...args: any[]): void
-  info(message?: any, ...args: any[]): void
-  log(message?: any, ...args: any[]): void
-  debug(message?: any, ...args: any[]): void
-  trace(message?: any, ...args: any[]): void
-  enabledFor(level: keyof L): boolean
-}
-
-export type Logger<L extends BaseLevels = BaseLevels> = BaseLogger<L> & {
-  [P in keyof Omit<L, keyof BaseLevels>]: (message?: any, ...args: any[]) => void
-}
-
-export interface AnyLogger<L extends BaseLevels, T extends BaseLogger<L> = Logger<L>> {
-  /**
-   * Returns an object containing all loggers created so far, keyed by name.
-   */
-  (): { [name: string]: T }
-
-  /**
-   * @param name The name of the new logger.
-   * @param config An optional config object.
-   * @returns A logger with the given `name` and `config`.
-   */
-  (name: string, config?: object | undefined): T
-
-  /**
-   * An object containing a mapping of level names to level values.
-   */
-  levels: L & { [name: string]: number }
-
-  /**
-   * Creates a new logger function that calls `anylogger.log` when invoked.
-   *
-   * @param name The name of the new logger.
-   * @param config An optional config object.
-   * @returns A new logger function with the given `name`.
-   */
-  new(name: string, config?: object | undefined): T
-
-  /**
-   * The log function used by `anylogger.new`.
-   *
-   * @param name The name of the logger to use
-   * @param level The log level.
-   * @param message The (formatted) message or any data to log.
-   * @param [params] Additional log (formatting) arguments.
-   */
-  log(name: string, level: keyof L, message?: any, ...args: any[]): void
-  
-  /**
-   * The log function used by `anylogger.new`.
-   *
-   * @param name The name of the logger to use
-   * @param args The log arguments.
-   * @param message The (formatted) message or any data to log.
-   * @param [params] Additional log (formatting) arguments.
-   */
-  log(name: string, message?: any, ...args: any[]): void
-
-  /**
-   * @param logger The logger that should be (re-)extended.
-   * @return The logger that was given, extended.
-   */
-  ext(logger: T): T
-}
-
-declare const anylogger: AnyLogger<BaseLevels>
-
-export default anylogger
+// NOTE: Do not rewrite it into `export default` unless anylogger's `main`
+// entrypoint actually exports `default`.
+export = anylogger
